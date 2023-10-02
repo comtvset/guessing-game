4f4543af9fdfbc57065ca40bb27c19d9623a55bb
Vadzim Tsikhaniuk <comtvset@gmail.com>
2023-10-03 00:56:22 +0200

    fix: guessing-game is done (Tue, Oct 03, 2023 12:56:22 AM)

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let x = Math.ceil((this.min + this.max) / 2);
+        return x;
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
